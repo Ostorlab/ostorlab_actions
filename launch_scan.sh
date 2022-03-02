712b92dd9abb80bcf4c3090fb6216e5bc539185e
mohsinenar <mnarmouche@gmail.com>
2022-03-02 14:54:59 +0100

    update

--- launch_scan.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-set -e
-ostorlab ci-scan run --api-key="$INPUT_OSTORLAB_API_KEY" --title="$INPUT_SCAN_TITLE" --plan="$INPUT_PLAN" --type="$INPUT_TYPE" --file "$INPUT_FILE" --log_favor=github
